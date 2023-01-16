f25b0c6295b3362f21f649c267850a9947635ec2
M'hammed Tabarani <tabarani_mhammed@emsi-edu.ma>
2023-01-16 11:33:15 +0100

    last commit

---
+++ src/test/java/com/emsi/morocco24/PressAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.morocco24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
